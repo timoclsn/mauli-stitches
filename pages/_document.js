196b98d768fcfdf79ea943314f853046da6f1a08
Timo Clasen <goebeltimo@gmail.com>
2021-04-03 15:37:36 +0200

    Clean Project

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+import { getCssString } from '../stitches.config';
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <style
+                        id="stitches"
+                        dangerouslySetInnerHTML={{ __html: getCssString() }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
