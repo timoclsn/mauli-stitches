64a02bf114d4b2a86dfd3c2dcf5a9dac5912cfe2
Timo Clasen <goebeltimo@gmail.com>
2021-04-03 19:24:52 +0200

    Add SEO Information

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,65 @@ class MyDocument extends Document {
                         type="font/woff2"
                         crossOrigin="anonymous"
                     />
+                    <title>Mauli Design System</title>
+                    <meta
+                        name="description"
+                        content="Mauli Design System Playground"
+                        key="description"
+                    />
+                    <link
+                        rel="canonical"
+                        href="https://mauli.timoclasen.de"
+                        key="canonical"
+                    />
+
+                    <meta property="og:type" content="website" key="og:type" />
+                    <meta
+                        property="og:site_name"
+                        content="Mauli Design System"
+                        key="og:site_name"
+                    />
+                    <meta property="og:locale" content="de" key="og:locale" />
+                    <meta
+                        property="og:url"
+                        content="https://mauli.timoclasen.de"
+                        key="og:url"
+                    />
+                    <meta
+                        property="og:title"
+                        content="Mauli Design System"
+                        key="og:title"
+                    />
+                    <meta
+                        property="og:description"
+                        content="Mauli Design System Playground"
+                        key="og:description"
+                    />
+                    <meta
+                        name="twitter:card"
+                        content="summary_large_image"
+                        key="twitter:card"
+                    />
+                    <meta
+                        name="twitter:title"
+                        content="Mauli Design System"
+                        key="twitter:title"
+                    />
+                    <meta
+                        name="twitter:description"
+                        content="Mauli Design System Playground"
+                        key="twitter:description"
+                    />
+                    <meta
+                        name="twitter:site"
+                        content="@timoclsn"
+                        key="twitter:site"
+                    />
+                    <meta
+                        name="twitter:creator"
+                        content="@timoclsn"
+                        key="twitter:creator"
+                    />
                 </Head>
                 <body>
                     <Main />
